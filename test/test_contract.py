64d89d6e4df8d80a718d81b9010e49399fc1d93e
Philip Hayes <philiphayes9@gmail.com>
2016-02-14 03:28:28 -0800

    Fix a few more bugs in contract voting

--- test/test_contract.py
+++ test/test_contract.py
@@ -13,6 +13,12 @@ def to_ether(wei):
 def to_wei(ether):
     return ether * 10**18
 
+def approx(x, y, e):
+    return y - e <= x <= y + e
+
+def destroyed(c):
+    return self.assertEqual(c.get_recipient(123), 0)
+
 class TestContract(unittest.TestCase):
 
     def setUp(self):
@@ -70,6 +76,62 @@ class TestContract(unittest.TestCase):
         self.assertEqual(to_wei(1750), self.c.get_balance(123))
         self.assertEqual(to_wei(500), self.c.get_vote_position(123))
 
+    def test_vote_yes_rewards(self):
+        self.assertEqual(1, self.c.create_pool(123, t.a4, 'my contract', 'some event', 100, 50))
+
+        k4_bal = self.balance(t.a4)
+
+        # k1 commits 1000 eth
+        self.assertEqual(1, self.c.commit(123, sender=t.k1, value=to_wei(1000)))
+        self.assertEqual(to_wei(1000), self.c.get_balance(123))
+        # k2 commits 500 eth
+        self.assertEqual(1, self.c.commit(123, sender=t.k2, value=to_wei(500)))
+        self.assertEqual(to_wei(1500), self.c.get_balance(123))
+        # k3 commits 250 eth
+        self.assertEqual(1, self.c.commit(123, sender=t.k3, value=to_wei(250)))
+        self.assertEqual(to_wei(1750), self.c.get_balance(123))
+        self.assertEqual(0, self.c.get_vote_position(123))
+
+        # k1 votes yes and k4 is rewarded with ether
+        self.assertEqual(1, self.c.vote_yes(123, sender=t.k1))
+
+        k4_bal_after = self.balance(t.a4)
+        self.assertEqual(k4_bal + to_wei(1750), k4_bal_after)
+
+    def test_vote_no_rewards(self):
+        self.assertEqual(1, self.c.create_pool(123, t.a4, 'my contract', 'some event', 100, 50))
+
+        k1_bal = self.balance(t.a1)
+        k2_bal = self.balance(t.a2)
+        k3_bal = self.balance(t.a3)
+        k4_bal = self.balance(t.a4)
+
+        # k1 commits 1000 eth
+        self.assertEqual(1, self.c.commit(123, sender=t.k1, value=to_wei(1000)))
+        self.assertEqual(to_wei(1000), self.c.get_balance(123))
+        # k2 commits 500 eth
+        self.assertEqual(1, self.c.commit(123, sender=t.k2, value=to_wei(500)))
+        self.assertEqual(to_wei(1500), self.c.get_balance(123))
+        # k3 commits 250 eth
+        self.assertEqual(1, self.c.commit(123, sender=t.k3, value=to_wei(250)))
+        self.assertEqual(to_wei(1750), self.c.get_balance(123))
+        self.assertEqual(0, self.c.get_vote_position(123))
+
+        # k1 votes no and every peer is refunded
+        self.assertEqual(1, self.c.vote_no(123, sender=t.k1))
+
+        # assert destroyed(c)
+
+        k1_bal_after = self.balance(t.a1)
+        k2_bal_after = self.balance(t.a2)
+        k3_bal_after = self.balance(t.a3)
+        k4_bal_after = self.balance(t.a4)
+
+        # participants should be refunded
+        assert approx(k1_bal, k1_bal_after, to_wei(1))
+        assert approx(k2_bal, k2_bal_after, to_wei(1))
+        assert approx(k3_bal, k3_bal_after, to_wei(1))
+        assert approx(k4_bal, k4_bal_after, to_wei(1))
 
     def test_kill(self):
         self.c.create_pool(123, t.a0, 'my contract', 'some event', 100, 50)
