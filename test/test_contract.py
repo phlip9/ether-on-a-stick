be1e022b07780f2749e022c7edd5eb2f58e3dfd9
Philip Hayes <philiphayes9@gmail.com>
2016-02-13 22:49:45 -0800

    Fix strings

--- test/test_contract.py
+++ test/test_contract.py
@@ -14,19 +14,14 @@ class TestContract(unittest.TestCase):
         self.c = self.s.abi_contract('contract.se')
 
     def test_create_pool(self):
-        self.assertEqual(
-            self.c.create_pool(123, t.a0, 'my contract', 'some event', 100, 1234),
-            1
-        )
-        self.assertEqual(
-            self.c.get_recipient(123),
-            convert_hex_int(t.a0)
-        )
+        self.assertEqual(self.c.create_pool(123, t.a0, 'my contract', 'some event', 100, 1234), 1)
+        self.assertEqual(self.c.get_recipient(123), convert_hex_int(t.a0))
         self.assertEqual(self.c.get_name(123), 'my contract')
+        self.assertEqual(self.c.get_deadline(123), self.s.block.timestamp + 100)
+        self.assertEqual(self.c.get_voting_threshold(123), 1234)
+        self.assertEqual(self.c.get_balance(123), 0)
+        self.assertEqual(self.c.get_vote_position(123), 0)
 
-    # def test_vote_yes(self):
-        # state = tester.state()
-        # contract = state.abi_contract('contract.se')
 
 if __name__ == '__main__':
     unittest.main()
