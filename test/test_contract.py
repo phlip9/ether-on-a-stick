bd3945288cde6b76f3b9bf348b1447922cf9138c
Max Fang <maxfangx@gmail.com>
2016-02-13 20:51:35 -0800

    Testing boilerplate

--- test/test_contract.py
+++ test/test_contract.py
@@ -1,2 +1,12 @@
-def test_foo():
-    assert 1 == 1
+from ethereum import tester
+
+import unittest
+
+class TestContract(unittest.TestCase):
+
+    def test_vote_yes(self):
+        state = tester.state()
+        contract = state.abi_contract('contract.se')
+
+if __name__ == '__main__':
+    unittest.main()
