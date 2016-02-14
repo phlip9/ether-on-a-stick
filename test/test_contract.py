ea93b5744848bbfb391c6890e29a8201f23e3300
Max Fang <maxfangx@gmail.com>
2016-02-13 22:49:04 -0800

    More tests

--- test/test_contract.py
+++ test/test_contract.py
@@ -7,6 +7,7 @@ from ethereum import utils as u
 def convert_hex_int(hex_str):
     return int('0x' + hex_str.encode('hex'), 16)
 
+
 class TestContract(unittest.TestCase):
 
     def setUp(self):
@@ -22,11 +23,21 @@ class TestContract(unittest.TestCase):
             self.c.get_recipient(123),
             convert_hex_int(t.a0)
         )
-        self.assertEqual(self.c.get_name(123), 'my contract')
+        # self.assertEqual(self.c.get_name(123), 'my contract')
 
-    # def test_vote_yes(self):
-        # state = tester.state()
-        # contract = state.abi_contract('contract.se')
+    def test_commit(self):
+        self.c.create_pool(123, t.a0, 'my contract', 'some event', 100, 1234)
+        # self.s.send(t.a0, t.a1, 100000000)
+        self.s.send(t.k0, self.c.address, 1000000000000)
+        # self.c.commit(123)
+
+    def test_kill(self):
+        self.c.create_pool(123, t.a0, 'my contract', 'some event', 100, 1234)
+        ret = self.c.kill()
+
+    def test_vote_yes(self):
+        ret = self.c.create_pool(123, t.a0, 'my contract', 'some event', 100, 1234)
+
 
 if __name__ == '__main__':
     unittest.main()
