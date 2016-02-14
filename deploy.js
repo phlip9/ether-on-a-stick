639f6f18976c832c0e013eeceb96b356b1685871
Max Fang <maxfangx@gmail.com>
2016-02-14 01:55:56 -0800

    Script n shit

--- deploy.js
+++ deploy.js
@@ -1,17 +1,31 @@
+// To compile the serpent file, run
+// serpent mk_contract_info_decl contract.se
+var exec = require('child_process').exec;
+exec('serpent mk_contract_info_decl ether-on-a-stick/contract.se',
+    function (error, stdout, stderr) {
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+        var compiledContract = JSON.parse(stdout);
+        // console.log('stdout: ' + stdout);
+        // console.log('stderr: ' + stderr);
 
-var etherStickContract = web3.eth.contract(compiled.info.abiDefinition);
+        var etherStickContract = web3.eth.contract(compiledContract.info.abiDefinition);
 
-var etherStick = etherStickContract.new({from:web3.eth.accounts[0], data: compiled.code, gas: 300000}, function(e, contract){
-    if(!e) {
-      if(!contract.address) {
-        console.log("Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined...");
+        var etherStick = etherStickContract.new({from:web3.eth.accounts[0], data: compiledContract.code, gas: 300000}, function(e, contract){
+            if(!e) {
+              if(!contract.address) {
+                console.log("Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined...");
 
-      } else {
-        console.log("Contract mined! Address: " + contract.address);
-        console.log(contract);
-      }
+              } else {
+                console.log("Contract mined! Address: " + contract.address);
+                console.log(contract);
+              }
 
-    } else {
-        console.log(e);
-    }
-})
+            } else {
+                console.log(e);
+            }
+        })
+
+    });
+
---
+++ greeter.js
@@ -0,0 +1,33 @@
+var greeterSource = 'contract mortal { address owner; function mortal() { owner = msg.sender; } function kill() { if (msg.sender == owner) suicide(owner); } } contract greeter is mortal { string greeting; function greeter(string _greeting) public { greeting = _greeting; } function greet() constant returns (string) { return greeting; } }';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var greeterCompiled = web3.eth.compile.solidity(greeterSource);
+
+var _greeting = "Hello Max";
+var greeterContract = web3.eth.contract(greeterCompiled.greeter.info.abiDefinition);
+
+var greeter = greeterContract.new(_greeting,{from:web3.eth.accounts[0], data: greeterCompiled.greeter.code, gas: 300000}, function(e, contract){
+    if(!e) {
+
+      if(!contract.address) {
+        console.log("Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined...");
+
+      } else {
+        console.log("Contract mined! Address: " + contract.address);
+        console.log(contract);
+      }
+
+    }
+});
